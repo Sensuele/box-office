99ce128db38fa153f2ae0a36909bbf9fad3cf1e9
Евгений Дружняев <druzhniaev@gmail.com>
2021-01-03 17:46:42 +0600

    added show page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Show from './pages/Show';
 import Starred from './pages/Starred';
 
 function App() {
   return (
-      <Switch>
-        <Route exact path="/">
-          <Home/>
-        </Route>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-        <Route exact path="/starred">
-          <Starred/>
-        </Route>
-        <Route><div>not found page</div></Route>
-      </Switch>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+
+      <Route exact path="/show/:id">
+        <Show/>
+      </Route>
+
+      <Route>
+        <div>not found page</div>
+      </Route>
+    </Switch>
   );
 }
 
