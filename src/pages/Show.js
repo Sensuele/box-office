da060aa50d43e98d424a577e55b6d7d2b4b79500
Евгений Дружняев <druzhniaev@gmail.com>
2021-01-05 17:18:34 +0600

    created show page components

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,47 +1,54 @@
+/* eslint-disable no-underscore-dangle */
+
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
 const reducer = (prevState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case 'FETCH_SUCCESS': {
-      return {isLoading: false, error: null, show: action.show}
+      return { isLoading: false, error: null, show: action.show };
     }
 
     case 'FETCH_FAILED': {
-      return {...prevState, isLoading: false, error: action.error}
+      return { ...prevState, isLoading: false, error: action.error };
     }
 
-    default: return prevState
+    default:
+      return prevState;
   }
-}
-
+};
 
 const initialState = {
   show: null,
   isLoading: true,
-  error: null
-}
+  error: null,
+};
 
 const Show = () => {
   const { id } = useParams();
 
-  const [{show, isLoading, error}, dispatch] = useReducer(reducer, initialState);
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   useEffect(() => {
     let isMounted = true;
 
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(results => {
-          if (isMounted) {
-
-            dispatch( {type: 'FETCH_SUCCESS', show: results})
-            
-          }
+        if (isMounted) {
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
+        }
       })
       .catch(err => {
         if (isMounted) {
-          dispatch( {type: 'FETCH_FAILED', error: err.message})
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
         }
       });
 
@@ -58,6 +65,28 @@ const Show = () => {
     return <div>Error occured: {error}</div>;
   }
   console.log(show);
-  return <div>show page </div>;
+  return (
+    <div>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+      <div>
+        <h2>Details</h2>
+        <Details status={show.status} network={show.network} premiered={show.premierred} />
+      </div>
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+      <div>
+        <h2>Details</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
+    </div>
+  );
 };
 export default Show;
