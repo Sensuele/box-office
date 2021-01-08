9ea5acdd99d9bc59b1280a1e3e0b516fcd001f4e
Евгений Дружняев <druzhniaev@gmail.com>
2021-01-08 16:41:07 +0600

    optimize react components

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,17 +1,18 @@
-import { useReducer, useEffect, useState } from 'react';
+import { useReducer, useEffect, useState, useRef, useCallback } from 'react';
 import { apiGet } from './config';
 
 function showsReducer(prevState, action) {
-  switch(action.type) {
+  switch (action.type) {
     case 'ADD': {
-      return [...prevState, action.showId]
+      return [...prevState, action.showId];
     }
 
     case 'REMOVE': {
-      return prevState.filter((showId) => showId !== action.showId);
+      return prevState.filter(showId => showId !== action.showId);
     }
 
-    default: return prevState;
+    default:
+      return prevState;
   }
 }
 
@@ -23,8 +24,7 @@ function usePersistedReducer(reducer, initialState, key) {
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state))
-
+    localStorage.setItem(key, JSON.stringify(state));
   }, [state, key]);
 
   return [state, dispatch];
@@ -38,13 +38,16 @@ export function useLastQuery(key = 'lastQuery') {
   const [input, setInput] = useState(() => {
     const persisted = sessionStorage.getItem(key);
 
-    return persisted ? JSON.parse(persisted) : "";
+    return persisted ? JSON.parse(persisted) : '';
   });
 
-  const setPersistedInput = (newState) => {
-    setInput(newState);
-    sessionStorage.setItem(key, JSON.stringify(newState));
-  }
+  const setPersistedInput = useCallback(
+    newState => {
+      setInput(newState);
+      sessionStorage.setItem(key, JSON.stringify(newState));
+    },
+    [key]
+  );
 
   return [input, setPersistedInput];
 }
@@ -65,14 +68,11 @@ const reducer = (prevState, action) => {
 };
 
 export function useShow(showId) {
-  const [state, dispatch] = useReducer(
-    reducer,
-    {
-      show: null,
-      isLoading: true,
-      error: null,
-    }
-  );
+  const [state, dispatch] = useReducer(reducer, {
+    show: null,
+    isLoading: true,
+    error: null,
+  });
 
   useEffect(() => {
     let isMounted = true;
@@ -95,4 +95,39 @@ export function useShow(showId) {
   }, [showId]);
 
   return state;
+}
+
+export function useWhyDidYouUpdate(name, props) {
+  // Get a mutable ref object where we can store props ...
+  // ... for comparison next time this hook runs.
+  const previousProps = useRef();
+
+  useEffect(() => {
+    if (previousProps.current) {
+      // Get all keys from previous and current props
+      const allKeys = Object.keys({ ...previousProps.current, ...props });
+      // Use this object to keep track of changed props
+      const changesObj = {};
+      // Iterate through keys
+      allKeys.forEach(key => {
+        // If previous is different from current
+        if (previousProps.current[key] !== props[key]) {
+          // Add to changesObj
+          changesObj[key] = {
+            from: previousProps.current[key],
+            to: props[key],
+          };
+        }
+      });
+
+      // If changesObj not empty then output to console
+      if (Object.keys(changesObj).length) {
+        // eslint-disable-next-line no-console
+        console.log('[why-did-you-update]', name, changesObj);
+      }
+    }
+
+    // Finally update previousProps with current props for next hook call
+    previousProps.current = props;
+  });
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import ActorGrid from '../components/actor/ActorGrid';
 import CustomRadio from '../components/CustomRadio';
 import MainPageLayout from '../components/MainPageLayout';
@@ -10,6 +10,23 @@ import {
   SearchButtonWrapper,
   SearchInput,
 } from './Home.styled';
+
+
+const renderResults = (results) => {
+  if (results && results.length === 0) {
+    return <div>No results</div>;
+  }
+
+  if (results && results.length > 0) {
+    return results[0].show ? (
+      <ShowGrid data={results} />
+    ) : (
+      <ActorGrid data={results} />
+    );
+  }
+
+  return null;
+};
 
 const Home = () => {
   const [input, setInput] = useLastQuery();
@@ -24,9 +41,10 @@ const Home = () => {
     });
   };
 
-  const onInputChange = ev => {
+
+  const onInputChange = useCallback(ev => {
     setInput(ev.target.value);
-  };
+  }, [setInput]);
 
   const onKeyDown = ev => {
     if (ev.keyCode === 13) {
@@ -34,25 +52,13 @@ const Home = () => {
     }
   };
 
-  const onRadioChange = ev => {
+  const onRadioChange = useCallback(ev => {
     setSearchOption(ev.target.value);
-  };
+  }, []);
 
-  const renderResults = () => {
-    if (results && results.length === 0) {
-      return <div>No results</div>;
-    }
+ 
 
-    if (results && results.length > 0) {
-      return results[0].show ? (
-        <ShowGrid data={results} />
-      ) : (
-        <ActorGrid data={results} />
-      );
-    }
 
-    return null;
-  };
 
   return (
     <MainPageLayout>
@@ -67,24 +73,22 @@ const Home = () => {
       <RadioInputsWrapper>
         <div>
           <CustomRadio
-          label="Shows"
+            label="Shows"
             id="shows-search"
             value="shows"
             checked={isShowSearch}
             onChange={onRadioChange}
           />
-        
         </div>
         <div>
-        <CustomRadio
-          label="Actors"
-          id="actors-search"
-          type="radio"
-          value="people"
-          checked={!isShowSearch}
-          onChange={onRadioChange}
+          <CustomRadio
+            label="Actors"
+            id="actors-search"
+            type="radio"
+            value="people"
+            checked={!isShowSearch}
+            onChange={onRadioChange}
           />
-          
         </div>
       </RadioInputsWrapper>
       <SearchButtonWrapper>
@@ -93,7 +97,7 @@ const Home = () => {
         </button>
       </SearchButtonWrapper>
 
-      {renderResults()}
+      {renderResults(results)}
     </MainPageLayout>
   );
 };
